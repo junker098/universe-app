d1174b4091f2b9565dda2bd46029dfc14449473d
Yuriy <junker098@gmail.com>
2024-03-18 16:38:34 +0200

    Initial Commit

---
+++ Universe App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Universe App
+//
+//  Created by Yuriy on 18.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
