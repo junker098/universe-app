badf049600a29b8a58af362c489d76ec52bae445
Yuriy <junker098@gmail.com>
2024-03-21 11:06:22 +0200

    Add base ViewController for inheritance and alert presenters.

---
+++ Universe App/Core/BaseViewController.swift
@@ -0,0 +1,47 @@
+//
+//  BaseViewController.swift
+//  Universe App
+//
+//  Created by Yuriy on 20.03.2024.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    private var activityIndicator: UIAlertController?
+    
+    func showAlert(title: String?, message: String?, completionHandler: @escaping (Bool) -> Void) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { _ in
+            completionHandler(false)
+        }
+        
+        let deleteAction = UIAlertAction(title: "OK", style: .destructive) { _ in
+            completionHandler(true)
+        }
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(deleteAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    func showActivity() {
+        activityIndicator = UIAlertController(title: nil, message: "Please wait...", preferredStyle: .alert)
+        
+        let loadingIndicator = UIActivityIndicatorView(frame: CGRect(x: 10, y: 5, width: 50, height: 50))
+        loadingIndicator.hidesWhenStopped = true
+        loadingIndicator.style = UIActivityIndicatorView.Style.medium
+        loadingIndicator.startAnimating()
+        
+        guard let activityAlert = activityIndicator else { return }
+        activityAlert.view.addSubview(loadingIndicator)
+        present(activityAlert, animated: true, completion: nil)
+    }
+    
+    func dismissActivity() {
+        activityIndicator?.dismiss(animated: true)
+    }
+}
