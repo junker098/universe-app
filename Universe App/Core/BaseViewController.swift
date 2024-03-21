49993878c5b4f8deb829206076615ed7a3140525
Yuriy <junker098@gmail.com>
2024-03-21 19:27:43 +0200

    add simple alert for presenting error or etc information

--- Universe App/Core/BaseViewController.swift
+++ Universe App/Core/BaseViewController.swift
@@ -44,4 +44,11 @@ class BaseViewController: UIViewController {
     func dismissActivity() {
         activityIndicator?.dismiss(animated: true)
     }
+    
+    func showAllertMessage(text: String) {
+        let alertController = UIAlertController(title: "", message: text, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .cancel)
+        alertController.addAction(okAction)
+        present(alertController, animated: true, completion: nil)
+    }
 }
